5d4beee17f517a0fa05d917a56e86ce79d9b1518
Marc-Olivier Duceppe <marco_ul@hotmail.com>
2026-08-13 22:08:35 -0400

    Support C++11 firmware release build

--- include/measurement/CalibrationSettings.h
+++ include/measurement/CalibrationSettings.h
@@ -7,6 +7,17 @@ struct CurrentCalibration
     float shuntResistanceOhms = 0.015f;
     float shuntOffsetVolts = 0.0f;
     float currentGain = 1.0f;
+
+    CurrentCalibration() = default;
+    CurrentCalibration(
+        float shuntResistanceOhms,
+        float shuntOffsetVolts,
+        float currentGain)
+        : shuntResistanceOhms(shuntResistanceOhms),
+          shuntOffsetVolts(shuntOffsetVolts),
+          currentGain(currentGain)
+    {
+    }
 };
 
 class CalibrationSettings
